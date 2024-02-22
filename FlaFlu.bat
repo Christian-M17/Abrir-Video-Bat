4bc86213a14d15783aaf1b29104c470b56e530b7
Christian Ottero <62633531+Christian-M17@users.noreply.github.com>
2024-02-21 22:32:36 -0300

    Update FlaFlu.bat

--- FlaFlu.bat
+++ FlaFlu.bat
@@ -1,4 +1,4 @@
 for /l %%i in (1,1,99999999) do (
      timeout /t 2
-    start chrome "https://www.youtube.com/watch?v=hPtjWlNUjl0&t=372s" --new-window
+    start chrome "https://www.youtube.com/watch?v=hPtjWlNUjl0" --new-window
 )
